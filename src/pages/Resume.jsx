7263806fc059ba776b56188c1ba38bb1955db2ee
Alam Shaikh <alamshaikh9025@gmail.com>
2024-01-30 04:20:25 +0530

    Update resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,11 @@
 import { PDFViewer } from '@react-pdf/renderer';
 import MyDocument from '/alamresume.pdf'; 
-
+import alamresume from './alamresume.pdf'
 
   const Resume = () => {
     return(
         <section className='relative flex lg:flex-row flex-col max-container'>
-             <iframe src='../../public/alamresume.pdf' frameBorder="0" className='pdfview'> </iframe>
+             <iframe src={alamresume} frameBorder="0" className='pdfview'> </iframe>
          </section>
             
     );
